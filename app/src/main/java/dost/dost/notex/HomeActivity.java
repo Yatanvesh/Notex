e3e91a2841eaaed35446cefb50b02ee9abeb23fb
yatanvesh <yatan.vesh@gmail.com>
2019-01-22 19:36:20 +0530

    intial commit

---
+++ app/src/main/java/dost/dost/notex/HomeActivity.java
@@ -0,0 +1,14 @@
+package dost.dost.notex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
